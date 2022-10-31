412d494ae20b783e836be3d2f0c8cfd1eaeb2f50
Tolulope <toluadelekesteve@gmail.com>
2022-10-31 15:09:27 -0700

    update

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,18 @@
+.ready(() => {
+    let list = {}
+    $(".check").on("click", () => {
+        $(this).each(() => {
+            if (this.checked) {
+                list[$(this).data("id")] = $(this).data("name");
+            } else {
+                delete list.$(this).\data("id")
+            }
+        })
+    })
+
+    if (Object.values(list).length > 0) {
+        $(".amenities h4").text(Object.values(list).join(", "))
+    } else {
+        $(".amenities h4").html("&nbsp;")
+    }
+})
