7343fa6d544772614de9945e44ab0f9ee8ca4615
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-10-31 21:39:42 +0100

    Update 0-hbnb.py

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -25,7 +25,6 @@ def hbnb():
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
-    cache_id = uuid.uuid4()
 
     for state in states:
         st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
@@ -39,7 +38,7 @@ def hbnb():
     return render_template('100-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places, cache_id)
+                           places=places, cache_id = uuid.uuid4())
 
 
 if __name__ == "__main__":
